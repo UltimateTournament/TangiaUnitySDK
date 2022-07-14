55b330deb35fbce0de928869f34ef8f31e71aa04
Martin Rauscher <hades32@gmail.com>
2022-07-14 12:13:59 +0200

    first working version

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # Tangia Unity Game SDK
 
-This is the Unity SDK for integrating games with Tangia
+This is the Unity SDK for integrating games with Tangia. Tangia lets audiences interact directly
+with their streamers by paying for fun events to happen, customized items and more!
+
+You simply need to register<sup>*</sup> at https://tangia.co , create your games and list the 
+interactions/items that you want to offer. The IDs of the items you configure are passed to your
+game in events you receive with this SDK.
+
+_<sup>*</sup>currently in closed beta_
 
 ## Installation
 
@@ -8,3 +15,117 @@ This is the Unity SDK for integrating games with Tangia
 - Click the + at the top left of the Package Manager window
 - Select `Add package from git URL...`
 - Paste the following URL `https://github.com/UltimateTournament/ArcadeUnitySDK.git` and click `Add`
+
+## Usage
+
+Implementing our SDK consists of only 3 parts
+
+### Login
+
+You need to offer the content creator a way to configure their login.
+E.g. as a text box in your settings.
+
+> Make sure to enable pasting the code, so your players don't have to type out their codes
+
+These codes are one-time use: You send them to our API and get a session token that you need
+to store, so the player doesn't have to re-enter the code every time
+
+### Polling and Confirming Events
+
+When you have a session you have to poll our API for interaction events and for each event
+either acknowledge or reject so we know if it was successful.
+
+> You have to confirm events to get the next one
+
+### Stop Polling and Notify Game Stopped
+
+You should only be polling for events when the player is actively playing the game
+and interaction events _could_ be processed.
+You should not poll when they are in a menu or lobby screen because, so their audience doesn't try to 
+interact with them while it's not possible.
+
+This is also when you should mark the session as "not playing", so the audience gets instant feedback.
+
+### Complete Code Example
+
+Below you can see an example of how you could use our SDK in your code. The 
+`OnStreamerCodeEntered`, `OnStartPlaying` and `OnStopPlaying` could be called from your game as-is,
+and the `PollEvents` would need to actually interact with your game, e.g. by spawning new items.
+
+
+```cs
+using System.Collections;
+using UnityEngine;
+using Tangia;
+
+public class TangiaSpawner : MonoBehaviour
+{
+    // Get this ID from the Tangia game developer dashboard
+    // The version of your game is used to ensure we only send interaction events this version understands
+    TangiaAPI api = new TangiaAPI("game_abcdef123", "1.0.0");
+
+    // this should be stored permanently (e.g. on disk, or in browser local storage etc)
+    private string sessionKey = null;
+
+    public void OnStreamerCodeEntered(string code)
+    {
+        StartCoroutine(Login(code));
+    }
+
+    IEnumerator Login(string code)
+    {
+        LoginResult login = null;
+        yield return api.Login(code, lr => login = lr);
+        if (login.Success)
+        {
+            sessionKey = login.SessionKey;
+            api.SessionKey = sessionKey;
+            // we don't want code that waits for login to wait forever
+            StartCoroutine(nameof(PollEvents));
+        }
+        else
+        {
+            Debug.Log("login error: " + login.ErrorMessage);
+        }
+    }
+
+    public void OnStartPlaying()
+    {
+        if (sessionKey != null)
+        {
+            api.SessionKey = sessionKey;
+            StartCoroutine(nameof(PollEvents));
+        }
+    }
+
+    public void OnStopPlaying()
+    {
+        StopCoroutine(nameof(PollEvents));
+        StartCoroutine(api.StopPlaying());
+    }
+
+    private IEnumerator PollEvents()
+    {
+        while (true)
+        {
+            GameEventsResp resp = null;
+            yield return api.PollEvents(e => resp = e);
+            if (resp == null || resp.Events == null)
+            {
+                yield return new WaitForSeconds(0.2f);
+                continue;
+            }
+            foreach (var evt in resp.Events)
+            {
+                Debug.Log("we got an event: " + evt.ToString());
+                // TODO: check if we can process it right now and process it
+                //   e.g. by spawning a new item depending on `evt.InteractionID`
+                if (true)
+                    yield return api.AckEvent(evt.EventID);
+                else
+                    yield return api.RejectEvent(evt.EventID, "the reason why we rejected");
+            }
+        }
+    }
+}
+```
---
+++ Runtime/TangiaAPI.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections;
+using UnityEngine.Networking;
+using System.Text;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace Tangia
+{
+    public class TangiaAPI
+    {
+        private readonly string apiAddr;
+        private readonly string gameToken;
+        private readonly string gameVersion;
+
+        public string SessionKey { private get; set; }
+
+        /*
+	        
+	        
+	        
+	        /game/interactions/stop_playing
+         */
+
+        public TangiaAPI(string gameToken, string gameVersion)
+        {
+            this.apiAddr = Environment.GetEnvironmentVariable("TANGIA_ADDR") ?? "https://api.tangia.co";
+            this.gameToken = gameToken;
+            this.gameVersion = gameVersion;
+        }
+
+        public IEnumerator Login(string code, Action<LoginResult> callback)
+        {
+            return httpCall("POST", "/game/login", null, new GameLoginReq { GameID = gameToken, Code = code },
+                webReq => callback(new LoginResult { Success = true, SessionKey = JsonConvert.DeserializeObject<GameLoginResp>(webReq.text).SessionID }),
+                err => callback(new LoginResult { Success = false, ErrorMessage = err })
+                );
+        }
+
+        public IEnumerator PollEvents(Action<GameEventsResp> callback)
+        {
+            return httpCall("POST", "/game/interactions/poll", SessionKey, new GameEventsReq { GameVersion = gameVersion },
+                webReq => callback(JsonConvert.DeserializeObject<GameEventsResp>(webReq.text)),
+                err => callback(null)
+                );
+        }
+
+        public IEnumerator AckEvent(string eventID)
+        {
+            var result = new AckInteractionEventsReq { EventResults = new[] { new EventResult { EventID = eventID, Executed=true } } };
+            return httpCall("POST", "/game/interactions/ack", SessionKey, result,
+                webReq => { },
+                err => { }
+                );
+        }
+
+        public IEnumerator RejectEvent(string eventID, string reason)
+        {
+            var result = new AckInteractionEventsReq { EventResults = new[] { new EventResult { EventID = eventID, Executed = false, Message= reason } } };
+            return httpCall("POST", "/game/interactions/ack", SessionKey, result,
+                webReq => { },
+                err => { }
+                );
+        }
+
+        public IEnumerator StopPlaying()
+        {
+            return httpCall("POST", "/game/interactions/ack", SessionKey, null,
+                webReq => { },
+                err => { }
+                );
+        }
+
+        private IEnumerator httpCall(string method, string path, string authToken, object body, Action<DownloadHandlerBuffer> callback, Action<string> errorCallback)
+        {
+            using (var webReq = new UnityWebRequest(this.apiAddr + path, method))
+            {
+                var dl = new DownloadHandlerBuffer();
+                webReq.downloadHandler = dl;
+                if (authToken != null)
+                {
+                    webReq.SetRequestHeader("Authorization", "Bearer " + authToken);
+                }
+                if (body != null)
+                {
+                    var json = JsonConvert.SerializeObject(body);
+                    webReq.SetRequestHeader("Content-Type", "application/json");
+                    webReq.uploadHandler = new UploadHandlerRaw(Encoding.UTF8.GetBytes(json));
+                }
+                // for long-polling we need to set long timeouts
+                webReq.timeout = 60;
+                yield return webReq.SendWebRequest();
+
+                switch (webReq.result)
+                {
+                    case UnityWebRequest.Result.ConnectionError:
+                    case UnityWebRequest.Result.DataProcessingError:
+                    case UnityWebRequest.Result.ProtocolError:
+                        errorCallback("Error: " + webReq.error);
+                        break;
+                    case UnityWebRequest.Result.Success:
+                        if (webReq.responseCode >= 201 || webReq.responseCode >= 500 || webReq.responseCode == 429)
+                        {
+                            // just retry
+                            yield return new WaitForSeconds(1);
+                            var res = httpCall(method, path, authToken, body, callback, errorCallback);
+                            while (res.MoveNext())
+                            {
+                                yield return res.Current;
+                            }
+                        }
+                        else if (webReq.responseCode >= 400)
+                        {
+                            // we're assuming all other 4xx errors are permanent
+                            errorCallback("HTTP Status: " + webReq.responseCode);
+                        }
+                        else
+                        {
+                            callback(dl);
+                        }
+                        break;
+                }
+            }
+        }
+    }
+
+    class GameLoginReq
+    {
+
+        [JsonProperty(PropertyName = "GameID")]
+        public string GameID { get; set; }
+
+        [JsonProperty(PropertyName = "Code")]
+        public string Code { get; set; }
+    }
+    class GameLoginResp
+    {
+
+        [JsonProperty(PropertyName = "SessionID")]
+        public string SessionID { get; set; }
+    }
+
+    public class LoginResult
+    {
+        public bool Success { get; set; }
+        public string SessionKey { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+
+    public class GameEventsReq
+    {
+        [JsonProperty(PropertyName = "GameVersion")]
+        public string GameVersion { get; set; }
+    }
+    public class GameEventsResp
+    {
+        [JsonProperty(PropertyName = "Events")]
+        public GameEvent[] Events { get; set; }
+    }
+    public class GameEvent
+    {
+        [JsonProperty(PropertyName = "EventID")]
+        public string EventID { get; set; }
+
+        [JsonProperty(PropertyName = "InteractionID")]
+        public string InteractionID { get; set; }
+
+        [JsonProperty(PropertyName = "Price")]
+        public long Price { get; set; }
+
+        [JsonProperty(PropertyName = "BuyerName")]
+        public string BuyerName { get; set; }
+    }
+
+    public class AckInteractionEventsReq
+    {
+        [JsonProperty(PropertyName = "EventResults")]
+        public EventResult[] EventResults { get; set; }
+    }
+
+    public class EventResult
+    {
+        [JsonProperty(PropertyName = "EventID")]
+        public string EventID { get; set; }
+
+        [JsonProperty(PropertyName = "Executed")]
+        public bool Executed { get; set; }
+
+        [JsonProperty(PropertyName = "Message")]
+        public string Message { get; set; }
+    }
+
+}
